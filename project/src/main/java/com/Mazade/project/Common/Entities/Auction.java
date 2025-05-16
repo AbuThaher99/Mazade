050a5596934fad36900023e154385232b4ac328a
Mohammad AbuThaher <106724736+AbuThaher99@users.noreply.github.com>
2025-05-16 03:38:12 +0300

    update database desgin

--- project/src/main/java/com/Mazade/project/Common/Entities/Auction.java
+++ project/src/main/java/com/Mazade/project/Common/Entities/Auction.java
@@ -1,10 +1,16 @@
 package com.Mazade.project.Common.Entities;
+import com.Mazade.project.Common.Enums.Category;
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
+
 @Data
 @Builder
 @NoArgsConstructor
@@ -16,10 +22,14 @@ public class Auction extends BaseEntity {
     @Column(name = "winnerId")
     private Long winnerId;
 
-    @OneToOne
-    @JoinColumn(name = "postId")
-    @JsonBackReference("postAuction")
-    private Post post;
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY , orphanRemoval = true)
+    @JoinColumn(name = "auctionId", referencedColumnName = "id")
+    @JsonManagedReference("postAuction")
+    private List<Post> posts;
+
+    @Column(name = "Category", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Category category;
 
     @Column(name = "finalPrice")
     private double finalPrice;
--- project/src/main/java/com/Mazade/project/Common/Enums/ReactType.java
+++
@@ -1,7 +0,0 @@
-package com.Mazade.project.Common.Enums;
-
-public enum ReactType {
-    Excellent,
-    Good,
-    Bad
-}
