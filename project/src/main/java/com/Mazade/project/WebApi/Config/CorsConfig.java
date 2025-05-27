14c7d46dc74f6c27e21d6bbbbbad09ea07b3acdf
Mohammad AbuThaher <106724736+AbuThaher99@users.noreply.github.com>
2025-05-27 15:53:51 +0300

    update cors config

--- project/src/main/java/com/Mazade/project/WebApi/Config/CorsConfig.java
+++ project/src/main/java/com/Mazade/project/WebApi/Config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200","http://127.0.0.1:4200"
-                ,"http://localhost:3000","http://127.0.0.1:3000","http://127.0.0.1:5500","http://localhost:5500"));
+                ,"http://localhost:3000","http://127.0.0.1:3000","http://127.0.0.1:5500","http://localhost:5500","http://10.0.2.2:8080"));
         corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         corsConfiguration.setAllowCredentials(true);
